86c6b72451557f2a192eb1430ee581d9b5891307
Ketre3 <vash.19843@gmail.com>
2021-02-10 11:09:15 +0300

    plugins/torrent.py: refactor

--- plugins/torrent.py
+++ plugins/torrent.py
@@ -1,5 +1,7 @@
 # -*- coding: utf8 -*-
 #Developer by Bafomet
+from settings import torrent_api
+
 import requests
 #color
 R = '\033[31m'   # Red
@@ -8,23 +10,31 @@ C = '\033[1;32m' # Green
 W = '\033[0m'    # white
 O = '\033[45m'   # Purple
 
-def torrent(IP):
 
-    r = requests.get("https://api.antitor.com/history/peer/?ip="+ IP +"&key=3cd6463b477d46b79e9eeec21342e4c7")
+def torrent(IP):
+    r = requests.get(
+        "https://api.antitor.com/history/peer/",
+        params={"ip": IP, "key": torrent_api},
+    )
     res = r.json()
-    print (R + " [ + ]" + C + " Найдена информация по загрузкам Torrent." + "\n")
-    if len(res)>4:
-        print("  IP Address: "+res["ip"])
-        print("  ISP: "+res["isp"])
-        print("  Country: "+res["geoData"]["country"])
-        print("  Latitude: "+str(res["geoData"]["latitude"]))
-        print("  Longitude: "+str(res["geoData"]["longitude"])+"\n")
+    print(f"{R} [ + ]{C} Найдена информация по загрузкам Torrent." + "\n")
+
+    if len(res) > 4:
+        print(f"  IP Address: {res['ip']}")
+        print(f"  ISP: {res['isp']}")
+
+        geo_data = res['geoData']
+        print(f"  Country: {geo_data['country']}")
+        print(f"  Latitude: {geo_data['latitude']}")
+        print(f"  Longitude: {geo_data['longitude']}")
+        print()
+
         for i in res["contents"]:
-        	print("  Category:"+i["category"])
-        	print("  Name:"+i["name"])
-        	print("  Start:" + i["startDate"])
-        	print("  End:" + i["endDate"])
-        	print("  Size:"+str(i["torrent"]["size"]))
-        	print("")
+            print(f"  Category:{i['category']}")
+            print(f"  Name:{i['name']}")
+            print(f"  Start:{i['startDate']}")
+            print(f"  End:{i['endDate']}")
+            print(f"  Size:{i['torrent']['size']}")
+            print()
     else:
-        print(G + " [ + ]" + R + " Чет я нехуя нечего не нашел")
+        print(f"{G} [ + ]{R} Чет я нехуя нечего не нашел")
