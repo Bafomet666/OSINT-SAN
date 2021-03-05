79c9e218cf980f858c641cb029a8f2a8b8a04f51
Maxx <72593411+Bafomet666@users.noreply.github.com>
2021-03-05 14:09:09 +0000

    Add files via upload

--- module/utils/preload_banners.py
+++ module/utils/preload_banners.py
@@ -10,9 +10,9 @@ BANNER1 = '''
                                                                                     {3}⠄⠄⠄⠈⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠉⠄⠄⠄
  {1}[{2} Добро пожаловать в Framework.{1}]                                                   {3}⠄⠄⠄⠄⠄⠄⠄⠉⠛⠛⠛⠛⠛⠛⠛⠋⠉⠄⠄⠄⠄⠄⠄⠄
                                                                                     {3}⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
- {3}Author:{1}   [{2} bafomet  {1}]                                                             {3}⠄⡠⠄⠄⠄⢀⣠⣶⣿⣿⣿⣷⣼⣿⣿⣿⣶⣄⡀⠄⠄⠠⢄⠄
- {3}Version:{1}  [{2} 3.5.1    {1}]                                                             {3}⠄⣇⠄⢀⣴⣿⣿⣿⣿⣿⣿⡟⢻⣿⣿⣿⣿⣿⣿⣦⠄⠄⢸⠄
- {3}Country {1}  [{2} Russia{1}   ]                                                             {3}⠄⠈⠙⠛⠿⠿⠿⠿⠟⠛⠁⠄⠄⠈⠛⠻⠿⠿⠿⠿⠟⠋⠁⠄
+ {3}Author:{1}   [{2} Free    {1}]                                                               {3}⠄⡠⠄⠄⠄⢀⣠⣶⣿⣿⣿⣷⣼⣿⣿⣿⣶⣄⡀⠄⠄⠠⢄⠄
+ {3}Version:{1}  [{2} 3.5.1   {1}]                                                              {3}⠄⣇⠄⢀⣴⣿⣿⣿⣿⣿⣿⡟⢻⣿⣿⣿⣿⣿⣿⣦⠄⠄⢸⠄
+ {3}Country {1}  [{2} Russia{1}  ]                                                              {3}⠄⠈⠙⠛⠿⠿⠿⠿⠟⠛⠁⠄⠄⠈⠛⠻⠿⠿⠿⠿⠟⠋⠁⠄
  {3}Database:{1} [{2} 0{0} | {2}50.30 mb {1}] default                                                 {3}⠄⠄⠄⠄⠄⠄⣠⣶⣴⣤⡀⠄⠄⠄⣀⣤⣄⣦⡀⠄⠄⠄⠄⠄
  {3}API:     {1} [{2} on {1}]                                                                   {3}⠄⠄⠄⣀⣤⣾⣿⣿⣿⣿⣿⣷⣶⣿⣿⣿⣿⣿⣿⣦⣄⠄⠄⠄
                                                                                     {3}⣴⣾⣿⣿⣿⣿⣿⣿⣿⠿⠟⠋⠉⠉⠻⠿⣿⣿⣿⣿⣿⣿⣷⣦
@@ -22,28 +22,27 @@ BANNER1 = '''
  {3}У вас есть идеи по разработке ? Напишите нам)    '''                                   
 
 BANNER2 = '''
- {2}sudoodNo2~~Above.All.Esm⏣~~Destroy.No.Data~~s:cnv:odN  {1}  /$$$$$$   /$$$$$$  /$$$$$$ /$$   /$$ /$$$$$$$$    /$$$$$$   /$$$$$$  /$$   /$$
- {2}1UserheplmeM4f2c+h2~~Maintain.No.Persistence~~h+-h645  {1} /$$__  $$ /$$__  $$|_  $$_/| $$$ | $$|__  $$__/   /$$__  $$ /$$__  $$| $$$ | $$
- {2}398fhocnv:odNo2~~Above.All.Else.Do.No.Harm~~Ndo:76vc3  {1}| $$  \ $$| $$  \__/  | $$  | $$$$| $$   | $$     | $$  \__/| $$  \ $$| $$$$| $$
- {2}bafomet/gec/shadow.errorlis2n862.243.135.74.No.0MN89/  {1}| $$  | $$|  $$$$$$   | $$  | $$ $$ $$   | $$     |  $$$$$$ | $$$$$$$$| $$ $$ $$
- {2}osinttsan-in++e.AMd         .-://///+hbove.913.ElsMNh  {1}| $$  | $$ \____  $$  | $$  | $$  $$$$   | $$      \____  $$| $$__  $$| $$  $$$$
- {2}-~/.ssh/id_rsa.Des-                  `htN01UserWroteM  {1}| $$  | $$ /$$  \ $$  | $$  | $$\  $$$   | $$      /$$  \ $$| $$  | $$| $$\  $$$
- {2}:dopeAW.No<nano>o                     :is:TЯiKC.sudo-  {1}|  $$$$$$/|  $$$$$$/ /$$$$$$| $$ \  $$   | $$     |  $$$$$$/| $$  | $$| $$ \  $$
- {2}start3d.all.pyikeou                     The.PFYroy.No  {1} \______/  \______/ |______/|__/  \__/   |__/      \______/ |__/  |__/|__/  \__/
- {2}rp:PLACEDRINKHERE!:                          mdshell9 
- {2}:bafomet3422\                         :Ns.BOB&ALICEes                        {1}  [{2} Добро пожаловать в Framework.{1}]
- {2}pm---srwxrwx:-.m                        cMS146.52.No.
- {3}k54ТыscriptAc816/                        sENbove3101.    {3}Author:{1}   [{2} bafomet  {1}]
- {3}connect5836ITY.Do                        cT:/shSYSTEM    {3}Version:{1}  [{2} 3.5.1    {1}]
- {3}62.143.135.24aid                       /STFU|wall.No.    {3}Country {1}  [{2} Russia{1}   ]
- {3}:hevnsntSurb025N.                      dяжду32NG2GIV8    {3}Database:{1} [{2} 0{0} | {2}50.30 mb {1}] default
- {3}:#OUTHOUSE-  -s:                       /corysucses23D    {3}API:     {1} [{2} on {1}]
- {3}0:$nmap -oS                              SSo.6178306E              
- {3}9427nkad2g                           cnoaiurkfpajcwh;    {2}Благодарим вас за установку.   
- {3}:Ring0:                             3dDesbafometprodu    {2}Если вы используете бесплатную версию, обязательно введите все API.
- {3}:23d:                               sSETEC.ASTRONOMYi    {2}Обязательно выполните проверку зависимостей.
- {3}m/-                        /yo-    .98fjslmcq[pdigha2    {2}У вас есть идеи по разработке ? Напишите нам,
- {3}                            z:Shall.We.CyberPunk2077?    {2}мы придумаем как интегрировать вашу идею в framework.
- {3}                           632-ooy.if1ghtf0r+ehUser55    {2}Также после запуска ты обязуешься:
- {3}                         ..th3.realyU2VjRFNN.jMh+.f3f    {2}Соблюдать все правила написанные в лицензионном соглашении.
- '''
+ {1}  /$$$$$$   /$$$$$$  /$$$$$$ /$$   /$$ /$$$$$$$$    /$$$$$$   /$$$$$$  /$$   /$$  {2}sudoodNo2~~Above.All.Esm⏣~~Destroy.No.Data~~s:cnv:odN
+ {1} /$$__  $$ /$$__  $$|_  $$_/| $$$ | $$|__  $$__/   /$$__  $$ /$$__  $$| $$$ | $$  {2}1UserheplmeM4f2c+h2~~Maintain.No.Persistence~~h+-h645
+ {1}| $$  \ $$| $$  \__/  | $$  | $$$$| $$   | $$     | $$  \__/| $$  \ $$| $$$$| $$  {2}398fhocnv:odNo2~~Above.All.Else.Do.No.Harm~~Ndo:76vc3
+ {1}| $$  | $$|  $$$$$$   | $$  | $$ $$ $$   | $$     |  $$$$$$ | $$$$$$$$| $$ $$ $$  {2}bafomet/gec/shadow.errorlis2n862.243.135.74.No.0MN89/
+ {1}| $$  | $$ \____  $$  | $$  | $$  $$$$   | $$      \____  $$| $$__  $$| $$  $$$$  {2}osinttsan-in++e.AMd         .-://///+hbove.913.ElsMNh
+ {1}| $$  | $$ /$$  \ $$  | $$  | $$\  $$$   | $$      /$$  \ $$| $$  | $$| $$\  $$$  {2}-~/.ssh/id_rsa.Des-                  `htN01UserWroteM
+ {1}|  $$$$$$/|  $$$$$$/ /$$$$$$| $$ \  $$   | $$     |  $$$$$$/| $$  | $$| $$ \  $$  {2}:dopeAW.No<nano>o                     :is:TЯiKC.sudo-
+ {1} \______/  \______/ |______/|__/  \__/   |__/      \______/ |__/  |__/|__/  \__/  {2}start3d.all.pyikeou                     The.PFYroy.No
+                                                                                   {2}rp:PLACEDRINKHERE!:                          mdshell9
+ {1}  [{2} Добро пожаловать в Framework.{1}]                                                {2}:bafomet3422\                         :Ns.BOB&ALICEes
+                                                                                   {2}pm---srwxrwx:-.m                        cMS146.52.No.
+ {3}Author:{1}   [{2} Free    {1}]                                                             {1}k54ТыscriptAc816/                        sENbove3101.
+ {3}Version:{1}  [{2} 3.5.1   {1}]                                                             {1}connect5836ITY.Do                        cT:/shSYSTEM
+ {3}Country {1}  [{2} Russia{1}  ]                                                             {1}62.143.135.24aid                       /STFU|wall.No.
+ {3}Database:{1} [{2} 0{0} | {2}50.30 mb {1}] default                                                {1}:hevnsntSurb025N.                      dяжду32NG2GIV8
+ {3}API:     {1} [{2} on {1}]                                                                  {1}:#OUTHOUSE-  -s:                       /corysucses23D
+                                                                                   {1}0:$nmap -oS                              SSo.6178306E
+ {3}Благодарим вас за установку.                                                      {1}9427nkad2g                           cnoaiurkfpajcwh;
+ {3}Если вы используете бесплатную версию, обязательно введите все API.               {1}:Ring0:                             3dDesbafometprodu
+ {3}Обязательно выполните проверку зависимостей.                                      {1}:23d:                               sSETEC.ASTRONOMYi
+ {3}У вас есть идеи по разработке ? Напишите нам,                                      {1}m/-                        /yo-    .98fjslmcq[pdigha2
+ {3}мы придумаем как интегрировать вашу идею в framework.                             {1}                            z:Shall.We.CyberPunk2077?
+ {3}Также после запуска ты обязуешься:                                                {1}                           632-ooy.if1ghtf0r+ehUser55
+ {3}Соблюдать все правила написанные в лицензионном соглашении.                       {1}                         ..th3.realyU2VjRFNN.jMh+.f3f  '''
